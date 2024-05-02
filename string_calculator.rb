0d4d77e64fd0f4600ac1783c062db6037ea4fae5
Nithyanand <nithyanand@niyati.com>
2024-05-02 20:22:42 +0530

    changes to handle all possible scenarios

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,8 +1,22 @@
 class StringCalculator
 
   def self.add(string)
-    numbers_array = string.split(",").map(&:to_i)
-    numbers_array.sum
+    check_valid_input(string)
+    numbers_array = string.split(/[^[-]\d]/).map(&:to_i)
+    validate_negetive_numbers(numbers_array)
+    numbers_array.select { |num| num <= 1000 }.sum
+  end
+
+  private
+
+  def self.check_valid_input(string)
+    invalid_ip = string.split(',').include?("\n")
+    raise "Given input is Not ok" if invalid_ip
+  end
+
+  def self.validate_negetive_numbers(numbers_array)
+    negatives = numbers_array.select { |num| num.negative? }
+    raise "negatives not allowed #{negatives.join(',')}" if negatives.any?
   end
 
 end
--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -2,8 +2,60 @@ require_relative 'string_calculator'
 
 RSpec.describe StringCalculator do
   context '#add' do
-    it 'should return 0 when input is an empty string' do
-      expect(StringCalculator.add("")).to eq(0)
+    it 'should return the number when input is a single number' do
+      expect(StringCalculator.add('1')).to eq(1)
+    end
+
+    it 'should return the sum of two numbers when input is comma-separated numbers' do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
+
+    it 'should returns the sum of multiple numbers when input is comma-separated numbers' do
+      expect(StringCalculator.add('1,2,3,4')).to eq(10)
+    end
+
+    it 'should handles new lines between numbers' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'should throws an exception for input with only new line after comma ' do
+      expect { StringCalculator.add("1,\n") }.to raise_error("Given input is Not ok")
+    end
+
+    it 'should handles 2 digits and 3 digits numbers' do
+      expect(StringCalculator.add("1, 22, 333")).to eq(356)
+    end
+
+    it 'should supports different delimiters' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    it 'should supports different delimiters with 2 or 3 digits' do
+      expect(StringCalculator.add("//;\n11;222")).to eq(233)
+    end
+
+    it 'should throws an exception for negative numbers' do
+      expect { StringCalculator.add("-1,2,-3") }.to raise_error("negatives not allowed -1,-3")
+    end
+
+    it 'should ignores numbers greater than 1000' do
+      expect(StringCalculator.add("2,1001")).to eq(2)
+    end
+
+    it 'should supports delimiters of any length' do
+      expect(StringCalculator.add("//[***]\n1***2***3")).to eq(6)
+    end
+
+    it 'should supports delimiters of any length with 2 or 3 digits' do
+      expect(StringCalculator.add("//[***]\n1***44***333")).to eq(378)
+    end
+
+    it 'should supports multiple delimiters' do
+      expect(StringCalculator.add("//[*][%]\n1*2%3")).to eq(6)
+    end
+
+    it 'should supports multiple delimiters with length longer than one char' do
+      expect(StringCalculator.add("//[**][%%]\n1**2%%3")).to eq(6)
     end
   end
 end
